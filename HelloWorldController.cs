214d0e5ae45d5247ab4e902161e4f0dc1bdc6ef6
Alex Redding <Alexredding180@gmail.com>
2020-05-24 13:03:00 +0100

    Initial

---
+++ HelloWorldController.cs
@@ -0,0 +1,34 @@
+﻿using System.Web.Mvc;
+using TCAdmin.SDK.Web.MVC.Controllers;
+
+namespace Game
+{
+    public class HelloWorldModel
+    {
+        public string Message { get; set; }
+        public string ServiceStartedOn { get; set; }
+        public TCAdmin.GameHosting.SDK.Objects.Service Service { get; set; }
+    }
+
+    public class HelloWorldController : BaseServiceController
+    {
+        [HttpGet]
+        [ParentAction("Service", "Home")]
+        public ActionResult Index(int id)
+        {
+            //Make sure the user has permission this feature
+            EnforceFeaturePermission("FileManager");
+
+            var model = new HelloWorldModel();
+            //Gets the current service
+            model.Service = TCAdmin.GameHosting.SDK.Objects.Service.GetSelectedService();
+            //Sets the message variable
+            model.Message = "Hello World!";
+            //Gets information about the service startup
+            model.ServiceStartedOn = string.Format($"{model.Service.ConnectionInfo} was started on {TCAdmin.SDK.Misc.Dates.UniversalTimeToCurrentTimeZone(model.Service.Status.StartTime)} with process id {model.Service.Status.ProcessId}");
+
+
+            return View("HelloWorld", model);
+        }
+    }
+}
---
+++ ReadMe.txt
@@ -0,0 +1,11 @@
+﻿A Sample Custom Module made by M0RG4N & LFA
+
+How to install the module after compiling it:
+
+1. Copy SampleModule.dll to TCAdmin2\ControlPanel.MVC\bin-extensions
+
+2. Copy HelloWorld.cshtml to ControlPanel.MVC\Views\[ThemeFolder]\Game\Service
+
+3. Clear the contents of TCAdmin2\Cache and restart the monitor
+
+4. You can then navigate to the module in your browser e.g http://127.0.0.1/Service/HelloWorld/1
